c1f0fa1c79aa51bed2265d3de8dd33e5cdae159e
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2026-01-23 22:09:08 -0500

    Refactoring login method into a command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginToApplication', () => { 
+    cy.visit('/')
+    cy.contains('Sign in').click()
+    cy.get('[placeholder="Email"]').type(Cypress.env('loginEmail'))
+    cy.get('[placeholder="Password"]').type(Cypress.env('loginPassword'))
+    cy.contains('button', 'Sign in').click()
+ })
