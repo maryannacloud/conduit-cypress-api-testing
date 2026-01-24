30689012a157ec43675906522d423b4cbdc79832
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2026-01-23 23:50:24 -0500

    Modifying the response

--- cypress/e2e/api-test.cy.js
+++ cypress/e2e/api-test.cy.js
@@ -5,3 +5,14 @@ it('intercept api', () => {
   cy.intercept('GET', '**/articles*', {fixture: 'articles.json'})
   cy.loginToApplication()
 })
+
+it.only('modify api response', () => {
+  cy.intercept('GET', '**/articles*', req => {
+    req.continue(res => {
+      res.body.articles[0].favoritesCount = 999999
+      res.send(res.body)
+    })
+  })
+  cy.loginToApplication()
+  cy.get('app-favorite-button').first().should('contain.text', '999999')
+})
