d9f52c03da080991189ba0d451a11ef40d9cc00d
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2026-01-25 00:07:23 -0500

    Adding e2e test

--- cypress/e2e/api-test.cy.js
+++ cypress/e2e/api-test.cy.js
@@ -7,13 +7,13 @@ it('intercept api', () => {
 })
 
 // for more granular api interception configuration we can use route matcher
-it('intercept api - router matcher', () => {
+it('intercept api with router matcher', () => {
   cy.intercept({ method: 'GET', pathname: 'tags' }, { fixture: 'tags.json' })
   cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
   cy.loginToApplication()
 })
 
-it('modify api response', () => {
+it('intercept api and modify response', () => {
   cy.intercept('GET', '**/articles*', req => {
     req.continue(res => {
       res.body.articles[0].favoritesCount = 999999
@@ -39,10 +39,8 @@ it('waiting for apis', () => {
   })
 })
 
-it.only('delete newly created article', () => {
-
+it('delete newly created article', () => {
   const uniqueTitle = `AI Article ${Date.now()}`
-
   // Step 1: get the auth token
   cy.request({
     url: 'https://conduit-api.bondaracademy.com/api/users/login',
@@ -56,7 +54,6 @@ it.only('delete newly created article', () => {
   }).then(loginResponse => {
     expect(loginResponse.status).to.equal(200)
     const accessToken = `Token ${loginResponse.body.user.token}`
-
     // Step 2: created a new article with the unique title via API
     cy.request({
       url: 'https://conduit-api.bondaracademy.com/api/articles',
@@ -77,7 +74,6 @@ it.only('delete newly created article', () => {
       expect(response.body.article.title).to.equal(uniqueTitle)
     })
   })
-
   cy.loginToApplication()
   cy.contains('AI is amazing').click()
   cy.intercept('GET', '**/articles*').as('articleApiCall')
@@ -86,4 +82,62 @@ it.only('delete newly created article', () => {
   cy.contains('button', 'Delete Article').first().click()
   cy.wait('@articleApiCall')
   cy.get('app-article-list').should('not.contain.text', 'AI is amazing')
+})
+
+it.only('e2e api test', () => {
+  const uniqueTitle = `AI Article ${Date.now()}`
+  cy.request({
+    url: 'https://conduit-api.bondaracademy.com/api/users/login',
+    method: 'POST',
+    body: {
+      "user": {
+        "email": Cypress.env('loginEmail'),
+        "password": Cypress.env('loginPassword')
+      }
+    }
+  }).then(loginResponse => {
+    expect(loginResponse.status).to.equal(200)
+    const accessToken = `Token ${loginResponse.body.user.token}`
+    cy.request({
+      url: 'https://conduit-api.bondaracademy.com/api/articles',
+      method: 'POST',
+      body: {
+        "article": {
+          "title": uniqueTitle,
+          "description": "AI is amazing",
+          "body": "This is a body",
+          "tagList": []
+        }
+      },
+      headers: {
+        'Authorization': accessToken
+      }
+    }).then(response => {
+      expect(response.status).to.equal(201)
+    })
+    cy.request({
+      method: 'GET',
+      url: 'https://conduit-api.bondaracademy.com/api/articles',
+      headers: {'Authorization': accessToken}
+    }).then(response => {
+      expect(response.status).to.equal(200)
+      expect(response.body.articles[0].title).to.equal(uniqueTitle)
+      const slugID = response.body.articles[0].title.slug
+
+      cy.request({
+        method: 'DELETE',
+        url: `https://conduit-api.bondaracademy.com/api/articles/${slugID}`,
+        headers: {'Authorization': accessToken}
+      }).then(response.status).to.equal(204)
+    })
+
+    cy.request({
+      method: 'GET',
+      url: 'https://conduit-api.bondaracademy.com/api/articles',
+      headers: {'Authorization': accessToken}
+    }).then(response => {
+      expect(response.status).to.equal(200)
+      expect(response.body.articles[0].title).to.not.equal(uniqueTitle)
+    })
+  })
 })
